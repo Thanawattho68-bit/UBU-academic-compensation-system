8e7a283af88be49aacac3b7093c49db6ab1bd53a
kitsada <kitsada.ta.68@ubu.ac.th>
2026-02-26 21:39:40 +0700

    ลบระบบจัดรอบ

--- routes/__init__.py
+++ routes/__init__.py
@@ -6,6 +6,5 @@ routes/__init__.py
 from .auth import auth_bp
 from .main import main_bp
 from .requests import requests_bp
-from .rounds import rounds_bp
 from .admin import admin_bp
 from .api import api_bp
